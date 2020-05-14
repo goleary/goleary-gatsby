b8bb3139a5040c8c47b73f39bd70766692c8231a
Gabe O'Leary <oleary.gabe@gmail.com>
2020-05-14 11:36:07 -0700

    feat(posts): added disqus for users comments

--- src/templates/post.js
+++ src/templates/post.js
@@ -1,37 +1,50 @@
 import React from "react";
 import { HelmetDatoCms } from "gatsby-source-datocms";
 import { graphql } from "gatsby";
+import { DiscussionEmbed } from "disqus-react";
+
 import Layout from "../components/layout";
 import Img from "gatsby-image";
 
-export default ({ data }) => (
-  <Layout>
-    <article className="sheet">
-      <HelmetDatoCms seo={data.datoCmsPost.seoMetaTags} />
-      <div className="sheet__gallery">
-        {data.datoCmsPost.coverImage && data.datoCmsPost.coverImage.fuil && (
-          <Img
-            fluid={data.datoCmsPost.coverImage.fluid}
-            style={{ margin: "auto", width: 400 }}
-          />
-        )}
-      </div>
-      <div className="sheet__inner">
-        <h1 className="sheet__title">{data.datoCmsPost.title}</h1>
-      </div>
-      <div
-        className="sheet__body"
-        dangerouslySetInnerHTML={{
-          __html: data.datoCmsPost.contentNode.childMarkdownRemark.html,
-        }}
-      />
-    </article>
-  </Layout>
-);
+export default ({ data }) => {
+  const disqusConfig = {
+    shortname: process.env.GATSBY_DISQUS_NAME,
+    config: {
+      identifier: data.datoCmsPost.slug,
+      title: data.datoCmsPost.title,
+    },
+  };
+  return (
+    <Layout>
+      <article className="sheet">
+        <HelmetDatoCms seo={data.datoCmsPost.seoMetaTags} />
+        <div className="sheet__gallery">
+          {data.datoCmsPost.coverImage && data.datoCmsPost.coverImage.fuil && (
+            <Img
+              fluid={data.datoCmsPost.coverImage.fluid}
+              style={{ margin: "auto", width: 400 }}
+            />
+          )}
+        </div>
+        <div className="sheet__inner">
+          <h1 className="sheet__title">{data.datoCmsPost.title}</h1>
+        </div>
+        <div
+          className="sheet__body"
+          dangerouslySetInnerHTML={{
+            __html: data.datoCmsPost.contentNode.childMarkdownRemark.html,
+          }}
+        />
+        <DiscussionEmbed {...disqusConfig} />
+      </article>
+    </Layout>
+  );
+};
 
 export const query = graphql`
   query PostQuery($slug: String!) {
     datoCmsPost(slug: { eq: $slug }) {
+      slug
       seoMetaTags {
         ...GatsbyDatoCmsSeoMetaTags
       }
