b25687124994c85fba2b8fce12b018196af1738b
Gabe O'Leary <oleary.gabe@gmail.com>
2022-03-16 11:15:19 -0700

    revert: remove gatsby-plugin-splitbee

--- gatsby-config.js
+++ gatsby-config.js
@@ -20,12 +20,5 @@ module.exports = {
         trackingId: "UA-165977347-1",
       },
     },
-    {
-      resolve: `gatsby-plugin-splitbee`,
-      options: {
-        includeInDevelopment: false,
-        delayTimeout: 0,
-      },
-    },
   ],
 };
