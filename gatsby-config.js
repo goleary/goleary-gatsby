76d6913f6fc9f91487505eafc4f085a055d34259
Gabe O'Leary <oleary.gabe@gmail.com>
2024-02-02 11:29:04 -0800

    add umami

--- gatsby-config.js
+++ gatsby-config.js
@@ -20,5 +20,17 @@ module.exports = {
         trackingId: "UA-165977347-1",
       },
     },
+    {
+      resolve: `gatsby-plugin-umami`,
+      options: {
+        websiteId: "3bb05bc8-9b50-4ed9-9a15-313979906ae2",
+        srcUrl: "https://umami-production-9fe7.up.railway.app/script.js",
+        includeInDevelopment: false,
+        autoTrack: true,
+        respectDoNotTrack: true,
+        dataCache: false,
+        dataDomains: "goleary.com,gabeoleary.com",
+      },
+    },
   ],
 };
